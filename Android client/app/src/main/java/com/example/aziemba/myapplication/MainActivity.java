2a3ca60a939f8c0dbf5f6908a5268cb800be2c96
Artur Ziemba <aziemba@global.avidww.com>
2017-09-05 08:46:09 +0200

    before the button test

--- Android client/app/src/main/java/com/example/aziemba/myapplication/MainActivity.java
+++ Android client/app/src/main/java/com/example/aziemba/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
