7b2637176f6761c06930e39f4fb4079b1ab42d4a
arogancki <ziembaart@gmail.com>
2018-02-14 20:29:19 +0100

    promise-extend tests

--- promise-extend/test/index.js
+++ promise-extend/test/index.js
@@ -1,16 +1,65 @@
 var rp = require('request-promise');
-require('../index.js')(Promise);
-/*
-Promise.last([rp('http://www.google.com'), rp('http://www.youtube.com'), rp('http://www.facebook.com')]).then(function(result){
-    console.log(result);
-})
-*/
-/*
-Promise.last([rp('http://www.google.com'), rp('http://www.youtube.com'), rp('http://www.facebook.com'), rp('http://asdadada.com/')]).then(function(result){
-    console.log(result);
-})
-*/
+Promise = require('../index.js')();
 
-Promise.race([rp('http://google.com/'), rp('http://')]).then(function(result){
-    console.log(result);
-})
+function promiseFactory(res, rej){
+	let promises = []
+	for (let i=0; i<res; i++)
+		promises.push(rp('http://www.google.com'))
+	for (let i=0; i<rej; i++)
+		promises.push(rp('http://www.gosdasdasdagshsr.com'),)
+	return promises
+}
+
+function doTest(fun, res, rej, m){
+	fun(promiseFactory(res,rej), m).then(()=>{
+		console.log("(S) res: " + fun.name + ": s:" + res + "/f:" + rej + (m?"/m:"+m:""))
+	}, ()=>{		
+		console.log("(F) rej: " + fun.name + ": s:" + res + "/f:" + rej + (m?"/m:"+m:""))
+	})
+}
+
+let tests1 = [
+	Promise.first,
+	Promise.last,
+	Promise.raceLast,
+	Promise.none,
+	Promise.any
+]
+
+let tests2 = [
+	Promise.few,
+	Promise.some
+]
+
+let params = [
+	{res:0, rej:0},
+	{res:1, rej:0},
+	{res:0, rej:1},
+	{res:3, rej:1},
+	{res:1, rej:3},
+	{res:3, rej:3}
+]
+
+for (let test of tests1)
+	for (let param of params)
+		doTest(test, param.res, param.rej)
+	
+for (let test of tests2)
+	for (let param of params)
+		doTest(test, param.res, param.rej)
+	
+for (let test of tests2)
+	for (let param of params)
+		doTest(test, param.res, param.rej, 1)
+
+for (let test of tests2)
+	for (let param of params)
+		doTest(test, param.res, param.rej, 2)
+	
+for (let test of tests2)
+	for (let param of params)
+		doTest(test, param.res, param.rej, 3)
+
+for (let test of tests2)
+	for (let param of params)
+		doTest(test, param.res, param.rej, 4)
