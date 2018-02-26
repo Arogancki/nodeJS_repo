039b3998c562ba472fc47be3b8438fd8ba25714d
Artur Ziemba <artur.ziemba@avid.com>
2018-02-26 18:14:45 +0100

    cQ#

---
+++ ConsoleApp3/ConsoleApp3/HtmlParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class HtmlParser
+    {
+        public static Parsed Parse(string tag, string doc) {
+
+            int tagOffset = tag.Length;
+            int start = doc.IndexOf("<"+tag+">");
+            if (start < 0)
+                throw new Exception("Invalid HTML table!");
+            start += (tagOffset + 2);
+            doc = doc.Substring(start);
+            int end = doc.IndexOf("</" + tag + ">");
+            if (end < 0)
+                throw new Exception("Invalid HTML table!");
+
+
+
+            string c = doc.Substring(0, end);
+            string o = doc.Substring(end + tagOffset + 3);
+            return new Parsed(c, o);
+        }
+            public class Parsed{
+            public string content = "";
+            public string offset = "";
+            public Parsed(string c, string o) {
+                this.content = c;
+                this.offset = o;
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = File.ReadAllText(@"./TextFile1.txt");
+            HtmlTable t = new HtmlTable(file);
+            string x = t.get();
+            Console.ReadKey();
+        }
+    }
+}
