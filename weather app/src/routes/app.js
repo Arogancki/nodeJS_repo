ca47b1f8b36f83d5d52a2a4bcffb17a4df6e04fa
arogancki <ziembaart@gmail.com>
2018-03-20 21:34:21 +0100

    weather app

--- weather app/src/routes/app.js
+++ weather app/src/routes/app.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 
     , send = require('../send')
     , html = require('../client/html')
+    , app = require('../client/app')
 
     , apiKey = process.env.API_KEY
 
@@ -19,11 +20,27 @@ async function getWeather(city){
     }));
 }
 
+
 module.exports=(passport)=>{    
     router.get('/', function (req, res) {
         // get user from db and populate to hisstory
         // get weather from last histrory city
         // and set it to derssion
+        let f = html(app(require('../mockData'), ''));
+        send(req, res, 200, f);
+        return
+        if (req.user.queries){
+            req.user.queries.populate('cities'), (err, cities)=>{
+                getWeather(cities[0]).then((data)=>{
+                    req.flash('background', data.weather.main);
+                    send(req, res, 200, html(app(data, '')));
+                }).catch(err=>{
+                    send(req, res, 200, html(app(undefined, err.message)));
+                })
+            }
+            return
+        }
+        send(req, res, 200, html(app(undefined, req.flash('appMessage')||"")));
     })
     router.post('/', function (req, res) {
         // add city to user hisotry
